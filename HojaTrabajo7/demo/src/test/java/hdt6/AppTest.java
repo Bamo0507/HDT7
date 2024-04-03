d990904de8d8eb9557fdd2df9b95933f6d37e85b
Bamo0507 <bryanalbertomo@gmail.com>
2024-04-02 18:24:47 -0600

    JUnits Hechos

--- HojaTrabajo7/demo/src/test/java/hdt6/AppTest.java
+++ HojaTrabajo7/demo/src/test/java/hdt6/AppTest.java
@@ -1,20 +1,42 @@
 package hdt6;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    private ArbolBinario<String> arbolBinario;
+
+    @Before
+    public void setUp() {
+        arbolBinario = new ArbolBinario<>();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testInsertarYBuscarAsociacionExistente() {
+        // Insertar una asociación en el árbol
+        arbolBinario.insertar(new String[]{"hello", "hola"});
+
+        // Buscar la asociación en el árbol
+        assertEquals("hola", arbolBinario.devolverEspanol("hello"));
+    }
+
+    @Test
+    public void testBuscarAsociacionNoExistente() {
+        // Buscar una asociación que no existe en el árbol
+        assertEquals("*goodbye*", arbolBinario.devolverEspanol("goodbye"));
+    }
+
+    @Test
+    public void testBuscarAsociacionExistente() {
+        // Insertar asociaciones en el árbol
+        arbolBinario.insertar(new String[]{"hello", "hola"});
+        arbolBinario.insertar(new String[]{"world", "mundo"});
+
+        // Buscar asociaciones en el árbol
+        assertEquals("mundo", arbolBinario.devolverEspanol("world"));
     }
 }
